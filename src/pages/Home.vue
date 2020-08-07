10271aec350c80096dddc3ce5532c9eead750395
Adam Alicki <aalicki@gmail.com>
2020-08-07 17:23:44 -0400

    Saving point for all data fetch flow.

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,6 +1,40 @@
 <template>
   <div class="home page">
     <h1>{{ msg }}</h1>
+    <div v-if="!this.dataFetched" class="loading">
+      Loading...
+    </div>
+    <hr>
+    <table v-if="this.dataFetched">
+      <thead>
+        <th>Coin Name</th>
+        <th>Symbol</th>
+        <th>Latest Price</th>
+        <th></th>
+      </thead>
+      <tbody>
+        <tr>
+          <td>BitCoin</td>
+          <td>BTC</td>
+          <td>${{this.coinData.BTC.price}}</td>
+        </tr>
+        <tr>
+          <td>BitCoin Cash</td>
+          <td>BCH</td>
+          <td>${{this.coinData.BCH.price}}</td>
+        </tr>
+        <tr>
+          <td>Tether</td>
+          <td>USDT</td>
+          <td>${{this.coinData.USDT.price}}</td>
+        </tr>
+        <tr>
+          <td>LiteCoin</td>
+          <td>LTC</td>
+          <td>${{this.coinData.LTC.price}}</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
@@ -15,6 +49,8 @@ export default {
   data () {
     return {
       msg: 'CrytpoTracker',
+      dataFetched: false,
+      events: [],
       bearer_token: '',
       fetchSymbolsMap: {
         BTC: true,
@@ -30,9 +66,51 @@ export default {
       }
     }
   },
+  created () {
+    axios.all([
+      this.setAuthorizationToken()
+    ])
+      .then(() => {
+        console.log('Done!')
+      })
+  },
   methods: {
-    // Fetch token asset information
-    fetchTokenID: function (fetchSymbol) {
+    setAuthorizationToken: function () {
+      // Get our Auth Token
+      axios({
+        method: 'POST',
+        url: 'https://bravenewcoin.p.rapidapi.com/oauth/token',
+        headers: {
+          'content-type': 'application/json',
+          'x-rapidapi-host': 'bravenewcoin.p.rapidapi.com',
+          'x-rapidapi-key': RAPID_API_KEY,
+          accept: 'application/json',
+          useQueryString: true
+        },
+        data: {
+          audience: 'https://api.bravenewcoin.com',
+          client_id: 'oCdQoZoI96ERE9HY3sQ7JmbACfBf55RY',
+          grant_type: 'client_credentials'
+        }
+      })
+        .then((response) => {
+          // Set our token
+          this.bearer_token = response.data.access_token
+        })
+        .then(() => {
+          // Set our token IDs
+          this.fetchCoinID('BCH')
+          this.fetchCoinID('BTC')
+          this.fetchCoinID('LTC')
+          this.fetchCoinID('USDT')
+        })
+        .catch((error) => {
+          console.log(error)
+        })
+    },
+
+    // Fetch Coin asset information
+    fetchCoinID: function (fetchSymbol) {
       axios({
         method: 'GET',
         url: 'https://bravenewcoin.p.rapidapi.com/asset',
@@ -53,15 +131,15 @@ export default {
           this.fetchSymbolsMap[fetchSymbol] = response.data.content[0].id
 
           // Fetch and update our coin data (maybe put this in a loop?)
-          this.fetchTokenData(fetchSymbol, this.fetchSymbolsMap[fetchSymbol])
+          this.fetchCoinData(fetchSymbol, this.fetchSymbolsMap[fetchSymbol])
         })
         .catch((error) => {
           console.log(error)
         })
     },
 
-    // Fetch the token information
-    fetchTokenData: function (symbol, symbolID) {
+    // Fetch the coin information
+    fetchCoinData: function (symbol, symbolID) {
       axios({
         method: 'GET',
         url: 'https://bravenewcoin.p.rapidapi.com/market-cap',
@@ -79,47 +157,16 @@ export default {
         .then((response) => {
           // update our coin data
           this.coinData[symbol] = response.data.content[0]
-          console.log(this.coinData[symbol])
         })
         .catch((error) => {
           console.log(error)
         })
+
+      console.log('Data All Fetched')
+      this.dataFetched = true
     }
   },
   mounted () {
-    // Get our Auth Token
-    axios({
-      method: 'POST',
-      url: 'https://bravenewcoin.p.rapidapi.com/oauth/token',
-      headers: {
-        'content-type': 'application/json',
-        'x-rapidapi-host': 'bravenewcoin.p.rapidapi.com',
-        'x-rapidapi-key': RAPID_API_KEY,
-        accept: 'application/json',
-        useQueryString: true
-      },
-      data: {
-        audience: 'https://api.bravenewcoin.com',
-        client_id: 'oCdQoZoI96ERE9HY3sQ7JmbACfBf55RY',
-        grant_type: 'client_credentials'
-      }
-    })
-      .then((response) => {
-        // Set our token
-        this.bearer_token = response.data.access_token
-
-        // Set our token IDs
-        this.fetchTokenID('BTC')
-        this.fetchTokenID('BCH')
-        this.fetchTokenID('LTC')
-        this.fetchTokenID('USDT')
-
-        // Now fetch the token information
-        // this.fetchTokenData()
-      })
-      .catch((error) => {
-        console.log(error)
-      })
   }
 }
 </script>
